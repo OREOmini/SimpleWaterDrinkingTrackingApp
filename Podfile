1c72583b675428f9288210bb5b6a7250e75c5964
OREOmini <brendawangwangwang@gmail.com>
2018-04-12 17:02:27 +0800

    init

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'drink' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for drink
+  pod 'KDCircularProgress'
+  pod 'SnapKit', '3.1.2'
+
+end
--- drink/ViewController.swift
+++ drink/ViewController.swift
@@ -7,19 +7,26 @@
 //
 
 import UIKit
+import SnapKit
+import KDCircularProgress
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setupElement()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func setupElement() {
+        
+    }
 
 }
 
