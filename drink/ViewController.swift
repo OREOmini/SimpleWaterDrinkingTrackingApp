f064200467e74293320ba5d22d8e782f97d0c638
OREOmini <brendawangwangwang@gmail.com>
2018-04-12 21:06:36 +0800

    初步完成

--- drink/ViewController.swift
+++ drink/ViewController.swift
@@ -14,10 +14,43 @@ import CoreData
 class ViewController: UIViewController {
     
     var circleView:KDCircularProgress?
-    var add50Button:AddButton?
-    var add100Button:AddButton?
+    var add50Button:CustomedButton?
+    var add100Button:CustomedButton?
+    var amountLabel:UILabel?
     
     var totalAmount:Int?
+    
+    var standardAmount = 2000
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let ud = UserDefaults.standard
+        let firstLaunch:Bool = ud.bool(forKey: "Launched");
+        if(!firstLaunch){
+            //第一次启动
+            print("first")
+            let app = UIApplication.shared.delegate as! AppDelegate
+            let context = app.persistentContainer.viewContext
+            
+            //创建User对象
+            let amount = NSEntityDescription.insertNewObject(forEntityName: "Amount",
+                                                           into: context) as! Amount
+            
+            //对象赋值
+            amount.drinkingAmount = 0
+            
+            //保存
+            do {
+                try context.save()
+                print("保存成功！")
+            } catch {
+                fatalError("不能保存：\(error)")
+            }
+        }else{
+            print("not first")
+            ud.setValue(true, forKey: "Launched")
+            ud.synchronize()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,44 +66,125 @@ class ViewController: UIViewController {
     }
     
     func setupElement() {
-        circleView = KDCircularProgress(frame: CGRect(x: 0, y: 0, width: 400, height: 400)).add(to: self.view)
+        circleView = KDCircularProgress(frame: CGRect(x: 0, y: 0, width: 400, height: 400))
+            .add(to: self.view)
+            .layout(snpMaker: { (make) in
+                make.center.equalToSuperview()
+                make.width.height.equalTo(400)
+        })
         circleView!.clockwise = true
-        circleView!.set(colors: .cyan)
+        circleView!.set(colors: .white)
         circleView!.glowMode = .forward
         circleView!.glowAmount = 0
-        circleView!.trackColor = UIColor.white
-        circleView!.angle = 0
+        circleView!.trackColor = UIColor.lightGray
+        circleView!.startAngle = -90
+        moveCircle()
         
-        let size = 50
+        let size = 70
         
-        add50Button = AddButton(frame: CGRect(x: 100, y: 100, width: size, height: size))
-        add50Button?.add(to: self.view)
+        amountLabel = UILabel().add(to: self.view)
+            .layout { (make) in
+                make.center.equalTo(circleView!)
+                make.edges.equalTo(circleView!)
+        }.config { (view) in
+            view.text = String(totalAmount!)
+            view.textAlignment = .center
+            view.textColor = .gray
+            view.font = UIFont.boldSystemFont(ofSize: 30)
+        }
+        
+        
+        CustomedButton(frame: CGRect(x: 0, y: 0, width: size, height: size)).add(to: self.view)
+            .layout { (make) in
+                make.centerX.equalToSuperview()
+                make.bottom.equalTo(circleView!.snp.top)
+                make.width.height.equalTo(size)
+        }.config { (view) in
+            view.setTitle("reset", for: .normal)
+            view.setTitleColor(.lightGray, for: .normal)
+            view.tintColor = UIColor.lightGray
+            view.addTarget(self, action: #selector(reset), for: .touchUpInside)
+
+        }
+        
+        
+        CustomedButton(frame: CGRect(x: 100, y: 100, width: size, height: size))
+            .add(to: self.view)
+            .layout(snpMaker: { (make) in
+                make.top.equalTo((circleView?.snp.bottom)!)
+                make.left.equalToSuperview().offset(60)
+                make.width.height.equalTo(size)
+        })
             .config({ (view) in
                 view.setTitle("+50", for: .normal)
                 view.addTarget(self, action: #selector(add50), for: .touchUpInside)
         })
         
-        add100Button = AddButton(frame: CGRect(x: 100, y: 300, width: size, height: size))
-        add100Button?.add(to: self.view)
+        
+        CustomedButton(frame: CGRect(x: 100, y: 100, width: size, height: size))
+            .add(to: self.view)
+            .layout(snpMaker: { (make) in
+                make.top.equalTo((circleView?.snp.bottom)!)
+                make.centerX.equalToSuperview()
+                make.width.height.equalTo(size)
+            })
             .config({ (view) in
                 view.setTitle("+100", for: .normal)
                 view.addTarget(self, action: #selector(add100), for: .touchUpInside)
+            })
+        
+        CustomedButton(frame: CGRect(x: 100, y: 300, width: size, height: size))
+            .add(to: self.view)
+            .layout(snpMaker: { (make) in
+                make.top.equalTo((circleView?.snp.bottom)!)
+                make.right.equalToSuperview().offset(-60)
+                make.width.height.equalTo(size)
+
+            })
+
+            .config({ (view) in
+                view.setTitle("+200", for: .normal)
+                view.addTarget(self, action: #selector(add200), for: .touchUpInside)
             })
 
     }
     
     func add50() {
-        
+        let temp = totalAmount! + 50
+        changeAmountTo(amount: temp)
     }
     func add100() {
-        
+        let temp = totalAmount! + 100
+        changeAmountTo(amount: temp)
+    }
+    func add200() {
+        let temp = totalAmount! + 200
+        changeAmountTo(amount: temp)
+
+    }
+    func reset() {
+        changeAmountTo(amount: 0)
+    }
+    func changeAmountTo(amount: Int) {
+        self.totalAmount = amount
+        moveCircle()
+        amountLabel?.text = String(amount)
+        changeCoreDataAmountTo(newAmount: totalAmount)
     }
     
-    func getAmount() -> Int? {
+    func moveCircle() {
+        let degree = Double(Double(totalAmount!) / Double(standardAmount)) * 360
+        circleView?.animate(toAngle: Double(degree), duration: 1, completion: { (completion) in
+            print("change to \(degree)")
+        })
+    }
+    
+    func getAmount() -> Int {
         let app = UIApplication.shared.delegate as! AppDelegate
         let context = app.persistentContainer.viewContext
         
         let fetchRequest = NSFetchRequest<Amount>(entityName:"Amount")
+        var num = 0
         
         do {
             let fetchedObjects = try context.fetch(fetchRequest)
@@ -78,14 +192,17 @@ class ViewController: UIViewController {
             //遍历查询的结果
             for info in fetchedObjects{
                 print("id=\(info.drinkingAmount)")
+                num = Int(info.drinkingAmount)
             }
         }
         catch {
             fatalError("不能保存：\(error)")
         }
+        
+        return num
     }
     
-    func changeAmountTo(newAmount: Int?) {
+    func changeCoreDataAmountTo(newAmount: Int?) {
         let app = UIApplication.shared.delegate as! AppDelegate
         let context = app.persistentContainer.viewContext
         
@@ -96,7 +213,7 @@ class ViewController: UIViewController {
             for info in fetchedObjects{
                 print("id=\(info.drinkingAmount)")
                 
-                amount.drinkingAmount = newAmount
+                info.drinkingAmount = Int16(newAmount!)
                 try context.save()
             }
             
@@ -119,12 +236,14 @@ class ViewController: UIViewController {
 
 }
 
-class AddButton: UIButton {
+class CustomedButton: UIButton {
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.layer.cornerRadius = frame.width / 2
-        self.layer.borderWidth =  2
-        self.layer.borderColor = UIColor.gray.cgColor
+        self.layer.borderWidth =  4
+        self.layer.borderColor = UIColor.lightGray.cgColor
+        self.setTitleColor(.lightGray, for: .normal)
+        self.tintColor = UIColor.lightGray
     }
     
     required init?(coder aDecoder: NSCoder) {
